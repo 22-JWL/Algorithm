e632db18bc78621a940cb3c44981711142259c5c
22-JWL <100843910+22-JWL@users.noreply.github.com>
2023-03-30 13:42:58 +0900

    [Bronze V] Title: A-B, Time: 144 ms, Memory: 15876 KB -BaekjoonHub

--- 백준/Bronze/1001. A－B/A－B.java
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -1,10 +1,33 @@
-import java.util.*;
-public class Main{
-	public static void main(String args[]){
-		Scanner sc = new Scanner(System.in);
-		int a, b;
-		a = sc.nextInt();
-		b = sc.nextInt();
-		System.out.println(a - b);
-	}
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+   public static void main(String[] args) throws IOException{
+      
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+      
+      StringTokenizer st;
+      
+//      int T = Integer.parseInt(br.readLine());
+      
+         
+         st = new StringTokenizer(br.readLine(), " ");//1 1가져옴(" "공백 기준으로 끊)
+         int A = Integer.parseInt(st.nextToken());
+         int B = Integer.parseInt(st.nextToken());
+         
+         bw.write( A - B + "\n");//글자라는것을알려주기 위해 가공  
+      
+      
+      bw.flush();
+      br.close();
+      bw.close();
+
+   }
+
 }
