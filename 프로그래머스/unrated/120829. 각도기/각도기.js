692ac3210b75da099421e22b8c1c8d5b70f7b083
22-JWL <100843910+22-JWL@users.noreply.github.com>
2023-10-12 17:08:12 +0900

    [level unrated] Title: 각도기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120829. 각도기/각도기.js
+++ 프로그래머스/unrated/120829. 각도기/각도기.js
@@ -1,12 +1,2 @@
-function solution(angle) {
-    
-    if(angle == 180){
-        return 4
-    }else if(90 < angle && angle < 180){
-        return 3
-    }else if(angle == 90){
-        return 2
-    }else if(0<angle && angle<90){
-        return 1
-    }
-}
+
+let solution = (angle) => (angle<90)?1:(angle==90)?2:(angle<180)?3:4
